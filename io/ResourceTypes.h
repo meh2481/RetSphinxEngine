3c012320c2675fb115f7cfa3a402170815055e7f
meh2481 <meh2481@email.vccs.edu>
2016-08-04 21:03:19 -0400

    Describe string bank file format

--- io/ResourceTypes.h
+++ io/ResourceTypes.h
@@ -73,5 +73,39 @@ typedef struct
 	//Followed by numChars * 8 floats (the UV coordinate rectangles for the characters, each float in range [0..1])
 } FontHeader;
 
+//--------------------------------------------------------------
+// Text data
+//--------------------------------------------------------------
+//String bank file format
+typedef struct
+{
+	uint32_t numStrings;
+	uint32_t numLanguages;
+	//Followed by numLangages LanguageOffsets
+	//Followed by numStrings StringIDs (sorted from least to greatest)
+	//Followed by numStrings*numLanguages StringPointers
+	//Followed by actual string data
+} StringBankHeader;
 
+//See https://www.loc.gov/standards/iso639-2/php/code_list.php for ISO 639 codes
+typedef struct
+{
+	uint8_t languageID[4];	//ISO 639-1 (if existing) or 639-2 language code in all-caps (EN for English, ES for Spanish, etc). Should only be two or three chars, others '\0'
+	uint32_t offset;		//Offset from first StringDataPointer for a string's ID to the StringDataPointer for this language
+
+//For example, for languages EN=0 and ES=1, for a StringID that's number 7 in the list (8th entry), 
+	//EN's StringDataPointer is number 7*2+0=14 and the English string can be found at that pointer's offset
+	//ES's StringDataPointer is number 7*2+1=15 and the Spanish string can be found at that pointer's offset
+
+} LanguageOffset;
+
+typedef struct
+{
+	uint64_t id;		//ID of the string
+} StringID;
+
+typedef struct
+{
+	uint64_t offset;	//Offset from start of file to the start of the actual, null-terminated string
+} StringDataPointer;
 
