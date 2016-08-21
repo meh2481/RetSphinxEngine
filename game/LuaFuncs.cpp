1ff339c71b967d84d9934b633cef1337e2aab570
meh2481 <meh2481@email.vccs.edu>
2016-08-21 17:28:08 -0400

    Genericize lua controller rumble

--- game/LuaFuncs.cpp
+++ game/LuaFuncs.cpp
@@ -25,113 +25,113 @@ public:
 	{
 		g_pGlobalEngine->rumbleController(fAmt, len, priority);
 	}
-	
+
 	static void camera_centerOnXY(Vec2 pt)
 	{
 		g_pGlobalEngine->CameraPos.x = -pt.x;
 		g_pGlobalEngine->CameraPos.y = -pt.y;
 	}
-	
-	static Object* xmlParseObj(string sClassName, Vec2 ptOffset = Vec2(0,0), Vec2 ptVel = Vec2(0,0))
+
+	static Object* xmlParseObj(string sClassName, Vec2 ptOffset = Vec2(0, 0), Vec2 ptVel = Vec2(0, 0))
 	{
 		Object* o = g_pGlobalEngine->getResourceLoader()->objFromXML(sClassName, ptOffset, ptVel);
 		if(o)
 			o->lua = g_pGlobalEngine->Lua;	//TODO Better way to load lua
 		return o;
 	}
-	
+
 	static void addObject(Object* o)
 	{
 		g_pGlobalEngine->getEntityManager()->add(o);
 	}
-	
+
 	static Object* getPlayerObject()
 	{
 		return g_pGlobalEngine->player;
 	}
-	
+
 	static void setPlayerObject(Object* o)
 	{
 		g_pGlobalEngine->player = o;
 	}
-	
+
 	static void loadMap(string sMap, string sNode = "")
 	{
 		g_pGlobalEngine->m_sLoadScene = sMap;
 		g_pGlobalEngine->m_sLoadNode = sNode;
 	}
-	
+
 	static bool keyDown(int key)
 	{
 		return g_pGlobalEngine->keyDown(key);
 	}
-	
+
 	static bool joyDown(int button)
 	{
 		if(g_pGlobalEngine->m_controller)
 			return (SDL_GameControllerGetButton(g_pGlobalEngine->m_controller, (SDL_GameControllerButton)button) > 0);
 		return false;
 	}
-	
+
 	static int joyAxis(int axis)
 	{
 		if(g_pGlobalEngine->m_controller)
 			return SDL_GameControllerGetAxis(g_pGlobalEngine->m_controller, (SDL_GameControllerAxis)axis);
 		return 0;
 	}
-	
+
 	static Vec2 getMousePos()
 	{
 		return g_pGlobalEngine->getCursorPos();
 	}
-	
+
 	static bool getMouseDown(int button)
 	{
 		return g_pGlobalEngine->getCursorDown(button);
 	}
-	
+
 	static Vec2 getWorldMousePos(Vec2 p)
 	{
 		return g_pGlobalEngine->worldPosFromCursor(p, g_pGlobalEngine->CameraPos);
 	}
-	
+
 	static ParticleSystem* createParticles(string sName)
 	{
 		ParticleSystem* pSys = g_pGlobalEngine->getResourceLoader()->getParticleSystem(sName);
 		g_pGlobalEngine->getEntityManager()->add(pSys);
 		return pSys;
 	}
-	
+
 	static LuaInterface* getLua()
 	{
 		return g_pGlobalEngine->Lua;
 	}
-	
+
 	static float getFramerate()
 	{
 		return g_pGlobalEngine->getFramerate();
 	}
-	
+
 	static Object* getObjAtPoint(Vec2 p)
 	{
 		return g_pGlobalEngine->getEntityManager()->getObject(p);
 	}
-	
+
 	static Node* getNodeAtPoint(Vec2 p)
 	{
 		return g_pGlobalEngine->getEntityManager()->getNode(p);
 	}
-	
+
 	static b2World* getPhysWorld()
 	{
 		return g_pGlobalEngine->getWorld();
 	}
-	
+
 	static Node* getNode(string sNodeName)
 	{
 		return g_pGlobalEngine->getEntityManager()->getNode(sNodeName);
 	}
-	
+
 	static Object* getClosestObject(Vec2 p)
 	{
 		return g_pGlobalEngine->getEntityManager()->getClosestObject(p);
@@ -175,15 +175,13 @@ luaFunc(controller_rumble)	//void controller_rumble(float force, float sec) --fo
 	luaReturnNil();
 }
 
-luaFunc(controller_lub)
+luaFunc(controller_rumbleLR) //void controller_rumbleLR(uint msec, float forceLarge, float forceSmall)
 {
-	GameEngineLua::rumbleLR(95, USHRT_MAX, 0);
-	luaReturnNil();
-}
+	int msec = lua_tointeger(L, 1);
+	float forceLarge = (float)lua_tonumber(L, 2);
+	float forceSmall = (float)lua_tonumber(L, 3);
 
-luaFunc(controller_dub)
-{
-	GameEngineLua::rumbleLR(32, 0, USHRT_MAX);
+	GameEngineLua::rumbleLR(msec, forceLarge * USHRT_MAX, forceSmall * USHRT_MAX);
 	luaReturnNil();
 }
 
@@ -207,7 +205,7 @@ luaFunc(getFramerate)	//float getFramerate()
 luaFunc(obj_setAngle)	//void obj_setAngle(obj* o, float angle)
 {
 	Object *o = getObj<Object>(L);
-	float f = (float)lua_tonumber(L,2);
+	float f = (float)lua_tonumber(L, 2);
 	if(o)
 	{
 		b2Body* b = o->getBody();
@@ -236,7 +234,7 @@ luaFunc(obj_getAngle)	//float obj_getAngle(obj* o)
 luaFunc(obj_setVelocity)	//void obj_setVelocity(obj* o, float xvel, float yvel)
 {
 	Object *o = getObj<Object>(L);
-	b2Vec2 p((float)lua_tonumber(L,2), (float)lua_tonumber(L, 3));
+	b2Vec2 p((float)lua_tonumber(L, 2), (float)lua_tonumber(L, 3));
 	if(o)
 	{
 		b2Body* b = o->getBody();
@@ -248,7 +246,7 @@ luaFunc(obj_setVelocity)	//void obj_setVelocity(obj* o, float xvel, float yvel)
 
 luaFunc(obj_getVelocity)	//float x, y obj_getVelocity(obj* o)
 {
-	b2Vec2 p(0,0);
+	b2Vec2 p(0, 0);
 	Object *o = getObj<Object>(L);
 	if(o)
 	{
@@ -262,7 +260,7 @@ luaFunc(obj_getVelocity)	//float x, y obj_getVelocity(obj* o)
 luaFunc(obj_applyForce)	//void obj_applyForce(obj* o, float x, float y)
 {
 	Object *o = getObj<Object>(L);
-	b2Vec2 pForce((float)lua_tonumber(L,2), (float)lua_tonumber(L, 3));
+	b2Vec2 pForce((float)lua_tonumber(L, 2), (float)lua_tonumber(L, 3));
 	if(o)
 	{
 		b2Body* b = o->getBody();
@@ -275,7 +273,7 @@ luaFunc(obj_applyForce)	//void obj_applyForce(obj* o, float x, float y)
 luaFunc(obj_getPos)		//float x, float y obj_getPos(obj* o)
 {
 	Object *o = getObj<Object>(L);
-	b2Vec2 pt(0,0);
+	b2Vec2 pt(0, 0);
 	if(o)
 	{
 		b2Body* bod = o->getBody();
@@ -288,7 +286,7 @@ luaFunc(obj_getPos)		//float x, float y obj_getPos(obj* o)
 luaFunc(obj_setPos)	//void obj_setPos(obj* o, float x, float y)
 {
 	Object *o = getObj<Object>(L);
-	Vec2 p(lua_tonumber(L,2), lua_tonumber(L,3));
+	Vec2 p(lua_tonumber(L, 2), lua_tonumber(L, 3));
 	if(o)
 		o->setPosition(p);
 	luaReturnNil();
@@ -296,10 +294,10 @@ luaFunc(obj_setPos)	//void obj_setPos(obj* o, float x, float y)
 
 luaFunc(obj_create) //obj* obj_create(string className, float xpos, float ypos, float xvel, float yvel)
 {
-	if(!lua_isstring(L,1)) luaReturnNil();
-	
-	Vec2 ptPos(0,0);
-	Vec2 ptVel(0,0);
+	if(!lua_isstring(L, 1)) luaReturnNil();
+
+	Vec2 ptPos(0, 0);
+	Vec2 ptVel(0, 0);
 	if(lua_isnumber(L, 2))
 		ptPos.x = (float)lua_tonumber(L, 2);
 	if(lua_isnumber(L, 3))
@@ -335,7 +333,7 @@ luaFunc(obj_registerPlayer)	//void obj_registerPlayer(obj* o)
 
 luaFunc(obj_getFromPoint) //obj* obj_getFromPoint(float x, float y)
 {
-	Vec2 p(lua_tonumber(L,1), lua_tonumber(L,2));
+	Vec2 p(lua_tonumber(L, 1), lua_tonumber(L, 2));
 	Object* o = GameEngineLua::getObjAtPoint(p);
 	if(o == NULL)
 		luaReturnNil();
@@ -374,9 +372,9 @@ luaFunc(obj_setImage)	//void obj_setImage(obj o, string sImgFilename, int seg = 
 		lua_Integer seg = 1;
 		if(lua_isinteger(L, 3))
 			seg = lua_tointeger(L, 3);
-		o->setImage(g_pGlobalEngine->getResourceLoader()->getImage(lua_tostring(L, 2)), (unsigned int)seg-1);	//Lua remains 1-indexed, C++ side 0-indexed
+		o->setImage(g_pGlobalEngine->getResourceLoader()->getImage(lua_tostring(L, 2)), (unsigned int)seg - 1);	//Lua remains 1-indexed, C++ side 0-indexed
 	}
-    luaReturnNil();
+	luaReturnNil();
 }
 
 //-----------------------------------------------------------------------------------------------------------
@@ -403,7 +401,7 @@ luaFunc(node_getProperty)	//string node_getProperty(Node* n, string propName)
 
 luaFunc(node_getVec2Property)	//float x, float y node_getVec2Property(Node* n, string propName)
 {
-	Vec2 pt(0,0);
+	Vec2 pt(0, 0);
 	Node* n = getObj<Node>(L);
 	if(n)
 		pt = pointFromString(n->getProperty(lua_tostring(L, 2)));
@@ -412,7 +410,7 @@ luaFunc(node_getVec2Property)	//float x, float y node_getVec2Property(Node* n, s
 
 luaFunc(node_getPos)	//float x, float y node_getPos(Node* n)
 {
-	Vec2 pos(0,0);
+	Vec2 pos(0, 0);
 	Node* n = getObj<Node>(L);
 	if(n)
 		pos = n->pos;
@@ -448,16 +446,16 @@ luaFunc(node_getCollidingObj) //obj[]* node_getCollidingObj(Node* n)
 				if(coll.nodeA == n && coll.objB != NULL)
 				{
 					//Push key/value pairs into table
-					lua_pushnumber(L,cur++);
+					lua_pushnumber(L, cur++);
 					lua_rawgetp(L, LUA_REGISTRYINDEX, coll.objB);
-					lua_settable(L,-3);
+					lua_settable(L, -3);
 				}
 				if(coll.nodeB == n && coll.objA != NULL)
 				{
 					//Push key/value pairs into table
-					lua_pushnumber(L,cur++);
+					lua_pushnumber(L, cur++);
 					lua_rawgetp(L, LUA_REGISTRYINDEX, coll.objA);
-					lua_settable(L,-3);
+					lua_settable(L, -3);
 				}
 			}
 		}
@@ -491,8 +489,8 @@ luaFunc(node_isInside) //bool node_isInside(Node* n, float x, float y)
 //-----------------------------------------------------------------------------------------------------------
 luaFunc(particles_create)	//ParticleSystem* particles_create(string sXMLFile)
 {
-	if(!lua_isstring(L,1)) luaReturnNil();
-	
+	if(!lua_isstring(L, 1)) luaReturnNil();
+
 	ParticleSystem* ps = GameEngineLua::createParticles(lua_tostring(L, 1));
 	LuaInterface* LI = GameEngineLua::getLua();
 	ps->glue = LI->createObject(ps, ps->TYPE, NULL);
@@ -574,7 +572,7 @@ luaFunc(mouse_transformToWorld) // float x, float y mouse_transformToWorld(int x
 
 luaFunc(mouse_setCursor)	//void mouse_setCursor(string cursorFile)
 {
-	if(!lua_isstring(L, 1)) 
+	if(!lua_isstring(L, 1))
 		luaReturnNil();
 
 	GameEngineLua::setCursor(lua_tostring(L, 1));
@@ -588,8 +586,7 @@ luaFunc(mouse_setCursor)	//void mouse_setCursor(string cursorFile)
 static LuaFunctions s_functab[] =
 {
 	luaRegister(controller_rumble),
-	luaRegister(controller_lub),
-	luaRegister(controller_dub),
+	luaRegister(controller_rumbleLR),
 	luaRegister(getFramerate),
 	luaRegister(obj_setVelocity),
 	luaRegister(obj_getVelocity),
@@ -630,34 +627,35 @@ static LuaFunctions s_functab[] =
 };
 
 //TODO: This is messed up; shouldn't be using SDL scancodes in Lua, shouldn't be defining joy axis min/max outside of where we define joy buttons
-static const struct {
+static const struct
+{
 	const char *name;
 	int value;
 } luaConstantTable[] = {
-	
+
 	//Joystick
 	luaConstant(JOY_AXIS_MIN),
 	luaConstant(JOY_AXIS_MAX),
-	
+
 	//Mouse
 	luaConstant(SDL_BUTTON_LEFT),
 	luaConstant(SDL_BUTTON_RIGHT),
 	luaConstant(SDL_BUTTON_MIDDLE),
 	luaConstant(SDL_BUTTON_FORWARD),
 	luaConstant(SDL_BUTTON_BACK),
-	
+
 	//Keyboard
 	luaConstant(SDL_SCANCODE_SPACE),
 };
 
 void lua_register_all(lua_State *L)
-{	
+{
 	//Register functions
 	for(unsigned int i = 0; s_functab[i].name; ++i)
 		lua_register(L, s_functab[i].name, s_functab[i].func);
-	
+
 	//Register constants
-	for (unsigned int i = 0; i < sizeof(luaConstantTable)/sizeof(*luaConstantTable); i++)
+	for(unsigned int i = 0; i < sizeof(luaConstantTable) / sizeof(*luaConstantTable); i++)
 	{
 		lua_pushinteger(L, luaConstantTable[i].value);
 		lua_setglobal(L, luaConstantTable[i].name);
