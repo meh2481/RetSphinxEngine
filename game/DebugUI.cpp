6afac83a5585da7dfab1345190d97a6b1e87c2e6
meh2481 <meh2481@email.vccs.edu>
2016-08-30 21:41:35 -0400

    Group/rename debug UI stuff

--- game/DebugUI.cpp
+++ game/DebugUI.cpp
@@ -6,11 +6,16 @@
 #include <climits>
 
 DebugUI::DebugUI(GameEngine *ge)
-: visible(false), hadFocus(false), _ge(ge), showTestWindow(false), rumbleMenu(true)
 {
+	visible = false;
+	hadFocus = false;
+	_ge = ge;
+	showTestWindow = false;
+	rumbleMenu = true;
+
 	largeMotorStrength = USHRT_MAX;
 	smallMotorStrength = USHRT_MAX;
-	motorDuration = 1000;
+	motorDuration = 100;
 	eventType = "ti_predefined_strongclick_100";
 	selectedSSMouseRumble = 1;
 	rumbleCount = 5;
@@ -24,11 +29,11 @@ DebugUI::~DebugUI()
 
 void DebugUI::draw()
 {
-	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.2f, 0.2f, 0.2f, 0.5f));
-	ImGui::PushStyleColor(ImGuiCol_ChildWindowBg, ImVec4(0.2f, 0.2f, 0.2f, 0.5f));
+	ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0.2f, 0.2f, 0.2f, 0.9f));
+	//ImGui::PushStyleColor(ImGuiCol_ChildWindowBg, ImVec4(0.2f, 0.2f, 0.2f, 0.9f));
 	_draw();
 	hadFocus = ImGui::IsMouseHoveringAnyWindow();
-	ImGui::PopStyleColor(2);
+	ImGui::PopStyleColor(1);
 }
 
 void DebugUI::_draw()
@@ -42,11 +47,11 @@ void DebugUI::_draw()
 		if(ImGui::BeginMenu("Windows"))
 		{
 #ifdef _DEBUG
-			ImGui::MenuItem("ImGui Test", NULL, &showTestWindow);
+			ImGui::MenuItem("ImGui Demo", NULL, &showTestWindow);
 #endif
-			ImGui::MenuItem("Memory debugger", NULL, &memEdit.Open);
+			ImGui::MenuItem("Memory Editor", NULL, &memEdit.Open);
 
-			ImGui::MenuItem("Gamepad rumble", NULL, &rumbleMenu);
+			ImGui::MenuItem("SteelSeries & Rumble", NULL, &rumbleMenu);
 
 			ImGui::EndMenu();
 		}
@@ -55,7 +60,7 @@ void DebugUI::_draw()
 	}
 
 	if(memEdit.Open)
-		memEdit.Draw("GameEngine memory", (unsigned char*)_ge, sizeof(*_ge));
+		memEdit.Draw("Memory Editor", (unsigned char*)_ge, sizeof(*_ge));
 
 #ifdef _DEBUG
 	if(showTestWindow)
@@ -64,29 +69,37 @@ void DebugUI::_draw()
 
 	if(rumbleMenu)
 	{
-		if(ImGui::Begin("Rumble Test", &rumbleMenu))
+		if(ImGui::Begin("SteelSeries & Rumble", &rumbleMenu))
 		{
-			ImGui::LabelText("", "Game controller rumble");
-			if(ImGui::Button("Test Controller Rumble"))
-				_ge->rumbleLR(motorDuration, largeMotorStrength, smallMotorStrength);
-			ImGui::SliderInt("large motor", &largeMotorStrength, 0, USHRT_MAX);
-			ImGui::SliderInt("small motor", &smallMotorStrength, 0, USHRT_MAX);
-			ImGui::SliderInt("duration (msec)", &motorDuration, 0, 5000);
-
-			ImGui::LabelText("", "SteelSeries mouse rumble");
-			if(ImGui::Button("Test Mouse Rumble"))
+			//Controller rumble testing
+			if(ImGui::CollapsingHeader("Game controller rumble"))
 			{
+				//ImGui::LabelText("", "Game controller rumble");
+				if(ImGui::Button("Test Controller Rumble"))
+					_ge->rumbleLR(motorDuration, largeMotorStrength, smallMotorStrength);
+				ImGui::SliderInt("large motor", &largeMotorStrength, 0, USHRT_MAX);
+				ImGui::SliderInt("small motor", &smallMotorStrength, 0, USHRT_MAX);
+				ImGui::SliderInt("duration (msec)", &motorDuration, 10, 5000);
+			}
+
+			//Mouse rumble testing
+			if(ImGui::CollapsingHeader("SteelSeries mouse rumble"))
+			{
+				//ImGui::LabelText("", "SteelSeries mouse rumble");
+				if(ImGui::Button("Test Mouse Rumble"))
+				{
 					static int eventVal = 0;
 					const char* TEST_EVNT = "TEST_EVNT2";
 					_ge->getSSCommunicator()->bindEvent(eventType, TEST_EVNT, rumbleFreq, rumbleCount, rumbleLen);
 					_ge->getSSCommunicator()->sendEvent(TEST_EVNT, ++eventVal);
+				}
+				ImGui::SliderInt("repeat", &rumbleCount, 1, 10);
+				ImGui::SliderFloat("frequency (Hz)", &rumbleFreq, 0.25, 5.0);
+				if(ImGui::Combo("rumble type", &selectedSSMouseRumble, steelSeriesTactileEvents, 124))
+					eventType = steelSeriesTactileEvents[selectedSSMouseRumble];
+				if(eventType == "custom")
+					ImGui::SliderInt("length (msec)", &rumbleLen, 1, 2559);
 			}
-			ImGui::SliderInt("repeat", &rumbleCount, 1, 10);
-			ImGui::SliderFloat("frequency (Hz)", &rumbleFreq, 0.25, 5.0);
-			if(ImGui::Combo("rumble type", &selectedSSMouseRumble, steelSeriesTactileEvents, 124))
-				eventType = steelSeriesTactileEvents[selectedSSMouseRumble];
-			if(eventType == "custom")
-				ImGui::SliderInt("length (msec)", &rumbleLen, 1, 2559);
 		}
 		ImGui::End();
 	}
