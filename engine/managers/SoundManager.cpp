ed0a2d9b5c6c33d7b806a42579d0dfb5b3de38a4
meh2481 <meh2481@email.vccs.edu>
2018-01-06 17:27:58 -0500

    For reals this time

--- engine/managers/SoundManager.cpp
+++ engine/managers/SoundManager.cpp
@@ -33,8 +33,8 @@ int SoundManager::init()
     if(version < FMOD_VERSION)
     {
         LOG(ERR) << "Error! You are using an old version of FMOD: " << std::hex
-            << (version & 0xFFFF0000) << '.' << (version & 0xFF00) << '.' << (version & 0xFF) << ". This program requires "
-            << (FMOD_VERSION & 0xFFFF0000) << '.' << (FMOD_VERSION & 0xFF00) << '.' << (FMOD_VERSION & 0xFF) << std::dec;
+            << ((version >> 16) & 0xFFFF) << '.' << ((version >> 8) & 0xFF) << '.' << (version & 0xFF) << ". This program requires "
+            << ((FMOD_VERSION >> 16) & 0xFFFF) << '.' << ((FMOD_VERSION >> 8) & 0xFF) << '.' << (FMOD_VERSION & 0xFF) << std::dec;
         return 1;
     }
     result = system->getNumDrivers(&numdrivers);
