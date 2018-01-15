7c2acd25e30033980e2d5db2d17866521ef3afc2
meh2481 <meh2481@email.vccs.edu>
2018-01-15 11:03:04 -0500

    Add FMOD DSP constants to Lua

--- res/lua/node/audiofilter.lua
+++ res/lua/node/audiofilter.lua
@@ -3,7 +3,7 @@ audiofilter.__index = audiofilter
 
 --Called when this node is created
 function audiofilter:init()
-	self.freq = tonumber(node_getProperty(self, "freq"))
+	local freq = tonumber(node_getProperty(self, "freq"))
 	local filtertype = node_getProperty(self, "filtertype")
 	self.dsp = audio_createFilter(FMOD_DSP_TYPE_MULTIBAND_EQ)
 	if filtertype == "highpass" then
@@ -11,10 +11,10 @@ function audiofilter:init()
 	else
 		audio_setFilterInt(self.dsp, FMOD_DSP_MULTIBAND_EQ_A_FILTER, FMOD_DSP_MULTIBAND_EQ_FILTER_LOWPASS_12DB)
 	end
-	audio_setFilterFloat(self.dsp, FMOD_DSP_MULTIBAND_EQ_A_FREQUENCY, self.freq)
+	audio_setFilterFloat(self.dsp, FMOD_DSP_MULTIBAND_EQ_A_FREQUENCY, freq)
 	self.x, self.y = node_getPos(self)
 	self.sizex, self.sizey = node_getVec2Property(self, "size")
-	audio_deactivateFilter(self.dsp)
+	audio_deactivateFilter(self.dsp)	--Deactivate to start
 end
 
 --Called when an object enters this node
@@ -27,8 +27,8 @@ function audiofilter:update(dt)
 	if player == nil then return end
 	local objx, objy = obj_getPos(player)
 	
-	--Don't divide by zero; if it's centered over us, just don't apply force this frame
-	--Also ignore if they're outside our radius (so we don't push away because math)
+	--Test if contains the center of player (Not precise but good enough for audio node)
+	--TODO: Sharp click when enable/disable abruptly like this. Prolly want to interpolate wet/dry or such
 	if rect_contains(objx, objy, self.x, self.y, self.sizex, self.sizey) then 
 		audio_activateFilter(self.dsp)
 	else
